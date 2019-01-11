1744b7be5e54bbdfbbc4f303cd439fdfb2dc18f8
T_Sharma <38181397+TanusreeSharma@users.noreply.github.com>
2019-01-11 05:25:09 -0600

    train_swapping commit

---
+++ train_swapping.c
@@ -0,0 +1,23 @@
+
+
+  #include<cstdio>
+
+  int main (){
+      int T[60], swap, i, j, n,m;
+      scanf("%d", &n);
+
+      while (n--){
+        scanf("%d", &m);
+        for(i=0; i < m; ++i)
+            scanf("%d", &T[i]);
+        //bubble sort
+        for(i = 0; i< m - 1; ++i){
+            for(j = i + 1; j < m; ++j){
+                if(T[i] > T[j])
+                    ++swap;
+            }
+        }
+        printf("Optimal train swapping takes %d swap.\n", swap);
+      }
+      return 0;
+  }
